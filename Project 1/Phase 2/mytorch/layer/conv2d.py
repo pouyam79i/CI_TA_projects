5ade8c8c1d4dcf09b7766dbb8fda3157e4baf8e6
Pouya <pouyamohammadyirbu@gmail.com>
2024-03-29 02:31:57 +0330

    update on layers

--- Project 1/Phase 2/mytorch/layer/conv2d.py
+++ Project 1/Phase 2/mytorch/layer/conv2d.py
@@ -1,14 +1,26 @@
 from mytorch import Tensor
 from mytorch.layer import Layer
 
+import numpy as np
+
 class Conv2d(Layer):
-    def __init__(self, in_channels, out_channels, name, kernel_size=(1, 1), stride=(1, 1), padding=(1, 1), initialize_method="random") -> None:
+    def __init__(self, in_channels, out_channels, kernel_size=(1, 1), stride=(1, 1), padding=(1, 1)) -> None:
         super()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.params = ...
         pass
 
     def forward(self, x: Tensor) -> Tensor:
         "TODO: implement forward pass"
         return ...
+    
+    def initialize(self): 
+        "TODO: initialize weights and bias"
+        pass
     
     def __str__(self) -> str:
         return "conv 2d"
--- Project 1/Phase 2/mytorch/layer/linear.py
+++ Project 1/Phase 2/mytorch/layer/linear.py
@@ -1,13 +1,22 @@
 from mytorch import Tensor
 from mytorch.layer import Layer
 
+import numpy as np
+
 class Linear(Layer):
-    def __init__(self) -> None:
+    def __init__(self, inputs: int, outputs: int) -> None:
+        self.inputs = inputs
+        self.outputs = outputs
+        self.params = ...
         pass
 
     def forward(self, x: Tensor) -> Tensor:
         "TODO: implement forward pass"
         return ...
+    
+    def initialize(self):
+        "TODO: initialize weights and bias"
+        pass
     
     def __str__(self) -> str:
         return "linear"
