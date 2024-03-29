c60e1441b62a9a0b2aacf792f8935a459f8ed755
Ali Bazshoushtari <abazshoushtari@gmail.com>
2024-03-29 17:28:28 +0330

    feat: implementing flatten function

--- Project 1/Phase 2/mytorch-framework/mytorch/util/flatten.py
+++ Project 1/Phase 2/mytorch-framework/mytorch/util/flatten.py
@@ -1,4 +1,17 @@
+import numpy as np
 from mytorch import Tensor
 
+
 def flatten(x: Tensor) -> Tensor:
-    pass
+    data = x.data.flatten()
+    req_grad = x.requires_grad
+    depends_on = x.depends_on
+
+    return Tensor(data=data, requires_grad=req_grad, depends_on=depends_on)
+
+
+# print(flatten(Tensor(np.array([
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]))))
