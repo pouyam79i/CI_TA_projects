ddab37e607d72d09dcd9beb1211fd8640183550c
Ali Bazshoushtari <abazshoushtari@gmail.com>
2024-04-01 19:02:04 +0330

    feat: adding hint and function structure for TODO

--- Project 1/Phase 2/mytorch/activation/leaky_relu.py
+++ Project 1/Phase 2/mytorch/activation/leaky_relu.py
@@ -2,5 +2,21 @@ import numpy as np
 from mytorch import Tensor, Dependency
 
 def leaky_relu(x: Tensor) -> Tensor:
-    "TODO: implement leaky relu function"
-    pass
+    """
+    fill 'data' and 'req_grad' and implement LeakyRelu grad_fn
+    hint: use np.where like Relu method but for LeakyRelu
+    """
+    # TODO: implement leaky_relu function
+
+    data = ...
+
+    req_grad = ...
+    if req_grad:
+        def grad_fn(grad: np.ndarray):
+            return None
+
+        depends_on = [Dependency(x, grad_fn)]
+    else:
+        depends_on = []
+
+    return Tensor(data=data, requires_grad=req_grad, depends_on=depends_on)
