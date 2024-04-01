fb46ba506d02d4e1e140aa7586f08729c7b78aa7
Ali Bazshoushtari <abazshoushtari@gmail.com>
2024-04-01 18:56:50 +0330

    fix: adding hint for TODO

--- Project 1/Phase 2/mytorch/activation/softmax.py
+++ Project 1/Phase 2/mytorch/activation/softmax.py
@@ -1,6 +1,12 @@
 import numpy as np
 from mytorch import Tensor, Dependency
 
+
 def softmax(x: Tensor) -> Tensor:
-    "TODO: implement softmax function"
+    # TODO: implement softmax function
+    # hint: you can do it using function you've implemented (not directly define grad func)
+    # hint: you can't use sum because it has not axis argument so there are 2 ways:
+    #        1. implement sum by axis
+    #        2. using matrix mul to do it :) (recommended)
+    # hint: a/b = a*(b^-1)
     pass
