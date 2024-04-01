dd1ad9d946a30a28aa459c0e3d4b5688d1f0dd1d
Ali Bazshoushtari <abazshoushtari@gmail.com>
2024-04-01 18:55:09 +0330

    fix: adding hint for TODO

--- Project 1/Phase 2/mytorch/activation/sigmoid.py
+++ Project 1/Phase 2/mytorch/activation/sigmoid.py
@@ -2,5 +2,6 @@ import numpy as np
 from mytorch import Tensor, Dependency
 
 def sigmoid(x: Tensor) -> Tensor:
-    "TODO: implement sigmoid function"
+    # TODO: implement sigmoid function
+    # hint: you can do it using function you've implemented (not directly define grad func)
     pass
