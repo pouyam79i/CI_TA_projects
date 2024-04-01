652cf2e8474abbbf3505031b5576e185574725a7
Ali Bazshoushtari <abazshoushtari@gmail.com>
2024-04-01 19:01:55 +0330

    feat: adding hint and function structure for TODO

--- Project 1/Phase 2/mytorch/activation/relu.py
+++ Project 1/Phase 2/mytorch/activation/relu.py
@@ -2,5 +2,18 @@ import numpy as np
 from mytorch import Tensor, Dependency
 
 def relu(x: Tensor) -> Tensor:
-    "TODO: implement relu function"
-    pass
+    # TODO: implement relu function
+
+    # use np.maximum
+    data = ...
+
+    req_grad = ...
+    if req_grad:
+        def grad_fn(grad: np.ndarray):
+            # use np.where
+            return None
+
+        depends_on = [Dependency(x, grad_fn)]
+    else:
+        depends_on = []
+    return Tensor(data=data, requires_grad=req_grad, depends_on=depends_on)
