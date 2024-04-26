f86f2d2354663c804552bdbf38419b1cbfe533a2
Pouya <pouyamohammadyirbu@gmail.com>
2024-04-26 16:27:53 +0330

    fix: pooling layers

--- Project 1/Phase 2/mytorch/layer/avg_pool2d.py
+++ Project 1/Phase 2/mytorch/layer/avg_pool2d.py
@@ -4,10 +4,8 @@ from mytorch.layer import Layer
 import numpy as np
 
 class AvgPool2d(Layer):
-    def __init__(self, in_channels, out_channels, kernel_size=(1, 1), stride=(1, 1), padding=(1, 1)) -> None:
+    def __init__(self, kernel_size=(1, 1), stride=(1, 1), padding=(1, 1)) -> None:
         super()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
